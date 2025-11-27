5b5c69d7fad17921566e854ea34bfe8ee45b421a
ValehK08 <valeh.kazimli@outlook.com>
2025-11-27 08:22:00 +0000

    6-print_comb3

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(1,99):
+    if i < 10:
+        print("{:02d}".format(i), end=", ")
+    if i == 89:
+        print("{:02d}".format(i))
+        break
+    if i >= 10 and i%10 > i//10:
+        print("{:02d}".format(i), end=", ")
