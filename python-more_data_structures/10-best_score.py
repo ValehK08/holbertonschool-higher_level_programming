38a260ccb4e7d4ecec73b3565e00083f4298f777
ValehK08 <valeh.kazimli@outlook.com>
2025-12-01 11:39:37 +0000

    Creation

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if len(a_dictionary) == 0:
+        return None
+    key, value = None, float('-inf')
+    for i in a_dictionary:
+        if a_dictionary[i] > value:
+            value = a_dictionary
+            key = i
+    return key
