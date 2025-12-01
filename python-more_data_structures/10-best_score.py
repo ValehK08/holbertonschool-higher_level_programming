e0270d9522664e46925f98ada3c87a72c64398bb
ValehK08 <valeh.kazimli@outlook.com>
2025-12-01 11:43:06 +0000

    Creation

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or a_dictionary is None:
+    if a_dictionary is None:
+        return None
+    elif len(a_dictionary) == 0:
         return None
     key, value = None, float('-inf')
     for i in a_dictionary:
