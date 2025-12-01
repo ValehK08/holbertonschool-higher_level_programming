eee8cc4a0a68b72eef86bb563ac5f0e386348d29
ValehK08 <valeh.kazimli@outlook.com>
2025-12-01 08:31:17 +0000

    2-args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 
 if __name__ == "__main__":
     import sys
-    print("{} arguments:".format(len(sys.argv)-1))
-    for i in range(1,len(sys.argv)):
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv)-1))
+    else:
+        print("{} arguments:".format(len(sys.argv)-1))
+    for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
