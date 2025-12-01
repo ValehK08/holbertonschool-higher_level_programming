2cdd83a12cbcd2f15f6c065d0c8375baf22438cb
ValehK08 <valeh.kazimli@outlook.com>
2025-12-01 10:43:55 +0000

    Creation

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.deepcopy()
-    for i in range(len(new_matrix)):
-        for j in range(len(new_matrix[i])):
-            new_matrix[i][j] = new_matrix[i][j] ** 2
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for j in range(len(matrix[i])):
+            new_matrix[i][j].append(matrix[i][j] ** 2)
     return new_matrix
