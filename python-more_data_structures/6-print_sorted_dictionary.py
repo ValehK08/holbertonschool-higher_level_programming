80504d2cc465e8d252d69a5b25b8ae7ee0d5f8ab
ValehK08 <valeh.kazimli@outlook.com>
2025-12-01 11:15:58 +0000

    Creation

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    a_dictionary = dict(sorted(my_dict.items()))
+    for item in a_dictionary:
+        print(f"{item}: {a_dictionary[item]}")
+
