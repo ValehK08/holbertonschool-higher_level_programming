b1f5169e167967da23a05b0ef77c00dc0df3f18e
ValehK08 <valeh.kazimli@outlook.com>
2025-12-01 11:21:26 +0000

    Creation

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a_dictionary = dict(sorted(my_dict.items()))
+    a_dictionary = dict(sorted(a_dictionary.items()))
     for item in a_dictionary:
         print(f"{item}: {a_dictionary[item]}")
 
