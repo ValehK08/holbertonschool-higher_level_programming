8862387c808f03c5967d232bccf9446d3583f8fa
ValehK08 <valeh.kazimli@outlook.com>
2025-12-01 11:33:12 +0000

    Creation

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] *= 2
-    return a_dictionary
+    a = a_dictionary.copy()
+    for i in a:
+        a[i] *= 2
+    return a
