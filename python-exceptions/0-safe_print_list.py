25962889da0f5232ff3d500222b53114ca044659
ValehK08 <valeh.kazimli@outlook.com>
2025-12-03 09:38:01 +0000

    .

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    ln = 0
+    for _ in my_list:
+        ln+=1
+    try:
+        if x > ln:
+            print(my_list[x])
+        for i in range(x):
+            print(my_list[i], end='')
+        print()
+        return x
+    except IndexError:
+        for i in my_list:
+            print(i, end='')
+        print()
+        return ln
