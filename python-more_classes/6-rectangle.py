4fe7c1db8f51687263b3f8d6625c711a5312bb4a
valeh.kazimli@outlook.com <valeh.kazimli@outlook.com>
2025-12-11 15:38:52 +0000

    .

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,13 +4,13 @@
 
 class Rectangle:
     """DEFINE IT"""
-    global number_of_instances
+
     number_of_instances = 0
     def __init__(self, width=0, height=0):
 
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -59,5 +59,5 @@ class Rectangle:
         return ("Rectangle({}, {})".format(self.__width, self.__height))
 
     def __del__(self):
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
