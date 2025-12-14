187457f9c7c124e307c72821d6f491388f2eb5fe
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:07:55 +0000

    .

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -14,11 +14,11 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
-    
+
     def area(self):
         """AREAAAAAA IMPLEMENTEEED"""
 
         return self.__width * self.__height
-    
+
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
