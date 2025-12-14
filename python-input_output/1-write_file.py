387628d03658c0bd31560de412d8cee1d0c557a4
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:34:53 +0000

    .

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,4 @@ def write_file(filename="", text=""):
     """FUNCTION TO WRITE FILE"""
 
     with open(filename, mode='w', encoding='utf-8') as f:
-        for line in f:
-            return f.write(text)
+        return f.write(text)
