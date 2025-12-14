d99ab92c5e0087d938fc490c67d96943e26334b7
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 14:06:20 +0000

    .

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+MY LIIIIIST MODULE
+"""
+
+
+class MyList(list):
+    """
+    MYLIIIIIIIST CLAASS
+    """
+
+    def print_sorted(self):
+        """
+        SORTING LIIIIIIIIIIIST
+        """
+
+        print(sorted(self))
