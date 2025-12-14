f21893de5d67eefc576652d7eb71e14b2702dec9
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 14:31:55 +0000

    .

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Base
+"""
+
+
+class BaseGeometry:
+    """
+    GEOOOOMETRY
+    """
+    
+    def area(self):
+        """
+        AREAAAA
+        """
+        
+        raise Exception("area() is not implemented")
