e52afc0b573ffaf7ff9bf320a8bd8e8e4cc1537f
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:04:48 +0000

    .

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,7 +6,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """RECTANGULATE ITTT"""
 
-
     def __init__(self, width, height):
         """INITIALIZE ITT"""
 
