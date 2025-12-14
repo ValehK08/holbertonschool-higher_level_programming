d1e38b5b69647de72f7c91f43441ca1cfa0c9241
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 16:15:05 +0000

    .

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -15,7 +15,7 @@ class Student:
     def to_json(self, attrs=None):
         if isinstance(attrs, list):
             if all(isinstance(i, str) for i in attrs):
-                return {key: obj_dict[key] for key in attrs}
+                return {key: self.__dict__[key] for key in attrs}
             else:
                 return self.__dict__
         else:
