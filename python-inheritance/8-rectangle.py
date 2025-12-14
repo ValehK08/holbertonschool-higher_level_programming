5429a8f1b5f484f2b88ce7400cce25a5bf1fce6b
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:01:36 +0000

    .

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,7 +2,27 @@
 """
 Base
 """
-from 7-base_geometry import BaseGeometry
+
+
+class BaseGeometry:
+    """
+    GEOOOOMETRY
+    """
+
+    def area(self):
+        """
+        AREAAAA
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        LEET'S VALIDATE
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
