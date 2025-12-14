78f5fe0aeab2079f0805685299b167ae154e018b
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:53:51 +0000

    .

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""READ THE JSON"""
+import json
+
+
+def load_from_json_file(filename):
+    """FUNCTION TO READ THE JSON"""
+
+    with open(filename, 'r') as file:
+            my_obj = json.load(file)
+    
+    return my_obj
