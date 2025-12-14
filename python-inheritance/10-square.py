4d9ce88ff90ac1b73a4df3c926422d727d1919fd
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:10:35 +0000

    .

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""DOCUMENTATION OF ITT"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """SQUAAAAAAARE IT"""
+
+    def __init__(self, size):
+        """VALIDATE ITT"""
+
+        self.integer_validator("size", size)
+        self.__size = size
