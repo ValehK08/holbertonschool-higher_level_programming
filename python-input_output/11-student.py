6bfcd55aee9ae0d3ae441ba6b59b2fcc6f41b16b
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 16:22:49 +0000

    .

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""CLASS: STUDENT"""
+
+
+class Student:
+    """STUDENT"""
+
+    def __init__(self, first_name, last_name, age):
+        """INITIALIZE"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            if all(isinstance(i, str) for i in attrs):
+                return {
+                    i: self.__dict__[i] for i in attrs if i in self.__dict__
+                    }
+            else:
+                return self.__dict__
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for i in json:
+            if i in self.__dict__.keys():
+                self.__dict__[i] = json[i]
