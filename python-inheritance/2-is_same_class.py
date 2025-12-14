fcc1d1468308e2dc5329ea8b0c66f3e02897d8ac
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 14:17:15 +0000

    .

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+IS IT THE SAME CLASSSSSSSS?????
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    I MADE A FUNCTION FOR IIIIIIT
+    """
+
+    return type(obj) == a_class
