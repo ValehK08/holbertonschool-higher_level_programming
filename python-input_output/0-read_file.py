bf36f65b77d2baf91a770e29bb15ffd5f8c77826
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:31:21 +0000

    .

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""READ FILE"""
+
+
+def read_file(filename=""):
+    """FUNCTION TO READ FILE"""
+    
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
