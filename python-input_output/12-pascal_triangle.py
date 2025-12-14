2e18e69c5f3dc4485112a6df454db4d3507a2ad6
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 16:36:32 +0000

    .

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""PASCAL"""
+
+
+def pascal_triangle(n):
+    init = [[1]]
+    if n > 1:
+        init.append([1, 1])
+    for i in range(2, n):
+        nxt = [1]
+        for k in range(i - 1):
+            nxt.append(init[i-1][k] + init[i-1][k+1])
+        nxt.append(1)
+        init.append(nxt)
+    for line in init:
+        print(line)
