e7d860b2ea3c9bd6f753e86ccaa3bd2a9dc4edc8
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:58:19 +0000

    .

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""READ THE JSON"""
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
+
+save_to_json_file(obj_json_file, 'add_item.json')
