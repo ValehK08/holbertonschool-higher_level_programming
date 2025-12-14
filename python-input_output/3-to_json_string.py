696c72917466a6aec94e9b42c72ddcb60f7a3917
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 15:40:08 +0000

    .

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """JSON TO STRING"""
-json = __import__('json').dumps
+import json
 
 def to_json_string(my_obj):
     """FUNCTION TO CONVERT JSON TO STRING"""
 
-    return dumps(my_obj)
+    return json.dumps(my_obj)
