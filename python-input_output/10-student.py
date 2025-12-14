80bf58739327d9f64b18f2c6920d55d61638d456
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 16:13:06 +0000

    .

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""CLASS: STUDENT"""
+
+
+class Student:
+    """STUDENT"""
+
+    def __init__(self, first_name, last_name, age):
+        """INITIALIZE"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if all(isinstance(i, str) for i in attrs):
+            return {key: obj_dict[key] for key in attrs}
+        else:
+            return self.__dict__
