589783d4a097d1b903669c7187ad774f617a536f
ValehK08 <valeh.kazimli@outlook.com>
2025-12-14 14:36:26 +0000

    .

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -14,3 +14,12 @@ class BaseGeometry:
         AREAAAA
         """
         raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        LEET'S VALIDATE
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
