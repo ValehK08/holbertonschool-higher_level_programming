59012f324022abf800bfccc48ed0257724f87026
ValehK08 <valeh.kazimli@outlook.com>
2025-12-15 06:02:47 +0000

    .

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- WHO SCORED THE MOST?? (only more than 10)
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC
