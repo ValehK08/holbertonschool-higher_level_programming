87a5df39c81bdc030d5239e248cd832a4fa3a693
ValehK08 <valeh.kazimli@outlook.com>
2025-12-19 11:28:52 +0000

    .

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,6 @@
 -- MY GENRES
-SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.id INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter';
